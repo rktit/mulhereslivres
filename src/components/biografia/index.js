be56e12b7f07bebde0a39d4373880316920bd2eb
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2022-05-21 12:34:38 -0300

    espaço texto

--- src/components/biografia/index.js
+++ src/components/biografia/index.js
@@ -50,13 +50,13 @@ export default function Page(props) {
                             </div>
                             <div className="flex flex-initial flex-col w-full descricao-bio-mob">
                                 <p>Para quem ainda não me conhece, eu sou a Dra. Amanda Almeida,
-                                    também conhecida como Dra. Vaginismo.</p>
+                                    também conhecida como Dra. Vaginismo.</p><br />
                                 <p>Eu sou formada em Fisioterapia pela UNESP (Universidade do Estado de São Paulo),
-                                    e especialista em Fisioterapia Pélvica e Sexologia, com foco na sáude íntima feminina.</p>
+                                    e especialista em Fisioterapia Pélvica e Sexologia, com foco na sáude íntima feminina.</p><br />
                                 <p>Há mais de 7 anos trabalho com mulheres com dor durante a relação sexual.
-                                    E há 2 anos criei o 1º tratamento de Vaginismo à distância do mundo, ajudando mais de 500 mulheres a se livrar do Vaginismo sem sair de casa e de vários lugares do mundo.</p>
+                                    E há 2 anos criei o 1º tratamento de Vaginismo à distância do mundo, ajudando mais de 500 mulheres a se livrar do Vaginismo sem sair de casa e de vários lugares do mundo.</p><br />
                                 <p>Diariamente compartilho muito conhecimento e qualidade de vida com milhares de
-                                    mulheres pelas redes sociais, principalmente através de meu perfil no <a href=" https://www.instagram.com/dra.vaginismo/" target="_blank">Instagram</a>.</p>
+                                    mulheres pelas redes sociais, principalmente através de meu perfil no <a href=" https://www.instagram.com/dra.vaginismo/" target="_blank">Instagram</a>.</p><br />
                                 <p>Tenho como propósito de vida levar a cura para milhões de mulheres que sofrem com
                                     essa disfunção, trazendo de volta a felicidade para a vida íntima de cada uma delas.</p>
                             </div>
