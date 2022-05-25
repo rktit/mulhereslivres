d23f2733bb757e5574eeb32d6ad6d66b4c70e638
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2022-05-25 17:42:40 -0300

    box-bonus removido

--- src/components/invista/index.js
+++ src/components/invista/index.js
@@ -6,6 +6,8 @@ import mais from '../../assets/file/mais.png'
 import botao from '../../assets/cta/botao-reservar.png'
 import garantia from '../../assets/cta/garantia.png'
 import valor from '../../assets/file/tabelaValor.png'
+import valorMobile from '../../assets/file/valorTabela.png'
+
 // import {ModalContext} from '../../context/index';
 
 export default function Page(props) {
@@ -58,30 +60,23 @@ export default function Page(props) {
                                     <div className="flex Tratamento py-2">
                                         <img src={mais} className="self-center pr-3 h-7" width="30" />
                                         Suporte pelo WhatsApp
-                                    </div> 
-                                    <div className="flex Tratamento py-2 pb-20">
+                                    </div>
+                                    <div className="flex Tratamento py-2 mb-10">
                                         <img src={mais} className="self-center pr-3 h-7" width="30" />
                                         Encontros mensais online<br /> com a Dra. Amanda Almeida.
                                     </div>
                                 </div>
                             </div>
-                            <div className="flex relative box-valor mb-10 ">
+                            
+                            <div className="flex relative box-valor">
+                            <img src={valorMobile} className="flex absolute pt-4" />
                                 <div className="flex absolute botao-invista justify-center">
                                     <img src={botao} className="flex invistaButton" />
                                     <button type="submit" className="absolute buttonInvista" onClick={() => { window.location.href = "https://sun.eduzz.com/1436910?cupom=sejalivre"; }}>Quero ser livre</button>
                                 </div>
                             </div>
 
-                                <div className="flex flex-col box-bonus text-center items-center place-self-center">
-                                    <div className="flex titulo-bonus pt-5">
-                                        Bônus
-                                    </div>
-                                    <div className="flex descricao-bonus pt-2">
-                                        Avaliação individual via vídeo<br /> chamada, com Dra. Amanda<br /> para as 5 primeiras inscritas.
-                                    </div>
-                            </div>
-
-                            <div className="relative flex justify-center pt-14 text-center pb-10">
+                            <div className="relative flex justify-center text-center pb-14 pt-20">
                                 <img src={garantia} className="flex button-garantia" />
                                 <div className="absolute titulo-garantia">Garantia de 7 Dias</div>
                                 <div className="absolute descricao-garantia"><br />
@@ -102,8 +97,8 @@ export default function Page(props) {
                             invista na sua liberdade!
                         </div>
                         <div className="flex flex-wrap justify-center">
-                            <div className="flex flex-col w-6/10 pt-12 pl-10">
-                                <div className="flex w-full justify-center invista py-2">
+                            <div className="flex flex-col pt-12 pl-20">
+                                <div className="flex w-full justify-start invista py-2">
                                     Tratamento de Vaginismo
                                 </div>
                                 <div className="flex flex-row w-full justify-center Tratamento py-2">
@@ -126,13 +121,25 @@ export default function Page(props) {
                                     <div className='flex flex-row justify-end'>
                                         <img src={mais} className="self-center pr-3 h-8" width="auto" />
                                     </div>
-                                    <div className='flex flex-row w-full justify-start'>
+                                    <div className='flex flex-row w-full justify-start pb-4'>
                                         Encontros mensais online<br /> com a Dra. Amanda Almeida.
                                     </div>
                                 </div>
+                                <div className="flex flex-wrap justify-center">
+                                    <div className="text-center">
+                                        <div className="relative flex justify-center pt-20">
+                                            <img src={garantia} className="flex button-garantia" />
+                                            <div className="absolute titulo-garantia">Garantia de 7 Dias</div>
+                                            <div className="absolute descricao-garantia"><br />
+                                                Sua compra está segura.<br />
+                                                Ou você fica satisfeita, ou recebe <br />
+                                                100% do seu investimento de volta.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-
-                            <div className="flex relative box-valor ml-28 mb-20">
+                            <div className="flex relative box-valor ml-28 pt-6">
                                 <img src={valor} className="flex absolute pt-4 pr-2" />
                                 <div className="flex absolute botao-invista  justify-center">
                                     <img src={botao} className="flex invistaButton" />
@@ -141,29 +148,6 @@ export default function Page(props) {
                             </div>
                         </div>
 
-                        <div className="flex flex-wrap justify-center">
-                            <div className="text-center pr-20">
-                                <div className="relative flex justify-center pt-14">
-                                    <img src={garantia} className="flex button-garantia" />
-                                    <div className="absolute titulo-garantia">Garantia de 7 Dias</div>
-                                    <div className="absolute descricao-garantia"><br />
-                                        Sua compra está segura.<br />
-                                        Ou você fica satisfeita, ou recebe <br />
-                                        100% do seu investimento de volta.
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="text-center pr-3">
-                                <div className="box-bonus">
-                                    <div className="titulo-bonus pt-5">
-                                        Bônus
-                                    </div>
-                                    <div className="descricao-bonus pt-2">
-                                        Avaliação individual via vídeo<br /> chamada, com Dra. Amanda para as 5<br /> primeiras inscritas.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </Fade>
                 </div>
             </ScrollableAnchor>
