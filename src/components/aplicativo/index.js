b6f5a11b02c51cb9c306fc8fccccbd58822bff40
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2022-08-09 10:17:08 -0300

    numero whats

--- src/components/aplicativo/index.js
+++ src/components/aplicativo/index.js
@@ -9,7 +9,7 @@ export default function Page(props) {
       <div className="fixed right-6 md:right-2 bottom-10 md:bottom-8 whats-message">
         <a
           target="_blank"
-          href="https://wa.me/message/TSSQOVGI3GRHK1"
+          href="https://wa.me/message/XYRDDYVQZI5UL1"
         >
           <img
             className="flex items-start md:items-end mr-2 md:mr-28 w-14 md:w-20"
